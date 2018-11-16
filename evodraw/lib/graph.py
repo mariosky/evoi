18b5f018a0ad4f62914e4a4394ff729cdf755dec
mariosky <mariosky@gmail.com>
2018-11-16 11:29:31 -0800

    Graph library

--- evodraw/lib/graph.py
+++ evodraw/lib/graph.py
@@ -10,35 +10,40 @@ class EvoGraph(object):
     def close(self):
         self._driver.close()
 
-
-    def create_user(self, username):
+    def create_actor(self, actorname):
         with self._driver.session() as session:
-            session.run("MERGE (u:User {name: $username})", username=username)
+            session.run("MERGE (u:Actor {name: $actorname})", actorname=actorname)
 
     def create_individual(self, individual):
         with self._driver.session() as session:
             session.run("MERGE (u:Individual {name: $individual})", individual=individual)
 
-
-    def like(self, username, individual):
+    def like(self, actorname, individual):
         with self._driver.session() as session:
-            session.write_transaction(self._like, username, individual)
+            session.write_transaction(self._like, actorname, individual)
 
-    def rate(self, username, individual, rating):
+    def rate(self, actorname, individual, rating):
         with self._driver.session() as session:
-            session.write_transaction(self._rate, username, individual, rating)
+            session.write_transaction(self._rate, actorname, individual, rating)
 
-    def is_child(self,  child, parent):
+    def add_child(self,  child, parent):
         with self._driver.session() as session:
             session.write_transaction(self._child,  child, parent)
 
-    def follows(self,  u1, u2):
+    def follows(self,  actor1, actor2):
         with self._driver.session() as session:
-            session.write_transaction(self._follows,  u1, u2)
+            session.write_transaction(self._follows,  actor1, actor2)
+
+    def actor_role(self, actorname, individual, relationship):
+        with self._driver.session() as session:
+            session.write_transaction(self._author_role, actorname, individual, relationship )
 
     def delete_all(self):
         with self._driver.session() as session:
             session.write_transaction(self._delete_all)
+
+
+
 
     @staticmethod
     def _delete_all(tx):
@@ -48,42 +53,54 @@ class EvoGraph(object):
     def _child(tx, child, parent):
         tx.run("MATCH (p:Individual {name: $parent})"
                "MATCH (c:Individual {name: $child}) "
-               "MERGE (c)-[:CHILD]->(p)",
+               "MERGE (c)-[:CHILD_OF]->(p)",
                child=child, parent=parent)
 
 
     @staticmethod
-    def _like(tx, username, individual):
-        tx.run("MATCH (u:User {name: $username})"
+    def _like(tx, actorname, individual):
+        tx.run("MATCH (u:Actor {name: $actorname})"
                "MATCH (i:Individual {name: $individual}) "
                "MERGE (u)-[:LIKES]->(i)",
-               username=username, individual=individual)
+               actorname=actorname, individual=individual)
 
     @staticmethod
-    def _rate(tx, username, individual, rating):
-        tx.run("MATCH (u:User {name: $username})"
+    def _rate(tx, actorname, individual, rating):
+        tx.run("MATCH (u:Actor {name: $actorname})"
                "MATCH (i:Individual {name: $individual})"
-               "MERGE (u)-[:RATING { rating: $rating } ]->(i)",
-               username=username, individual=individual, rating=rating)
+               "MERGE (u)-[:RATES { rating: $rating } ]->(i)",
+               actorname=actorname, individual=individual, rating=rating)
 
     @staticmethod
-    def _follows(tx, username1, username2):
-        tx.run("MATCH (u1:User {name: $username1})"
-               "MATCH (u2:User {name: $username2})"
-               "MERGE (u1)-[:FOLLOWS]->(u2)",
-               username1=username1, username2=username2)
+    def _author_role(tx, actorname, individual, relationship):
+        tx.run("MATCH (a:Actor {name: $actorname})"
+               "MATCH (i:Individual {name: $individual})"
+               "MERGE (a)-[_:%s]->(i)" % relationship ,
+               actorname=actorname, individual=individual, ROLE=relationship)
+
+    @staticmethod
+    def _follows(tx, actor1, actor2):
+        tx.run("MATCH (a1:Actor {name: $actor1})"
+               "MATCH (a2:Actor {name: $actor2})"
+               "MERGE (a1)-[:FOLLOWS]->(a2)",
+               actor1=actor1, actor2=actor2)
 
 
 
 if __name__ == "__main__":
     eg = EvoGraph( 'bolt://localhost:7687','neo4j', 'masterkey')
     eg.delete_all()
-    eg.create_user('Benzo')
-    eg.create_user('Washi')
+    eg.create_actor('Benzo')
+    eg.create_actor('Washi')
     eg.create_individual('pop:101')
     eg.create_individual('pop:102')
-    eg.is_child('pop:101', 'pop:102')
-    eg.like('Benzo','pop:102')
+    eg.create_individual('pop:103')
+    eg.add_child('pop:102', 'pop:101')
+    eg.add_child('pop:103', 'pop:101')
+    eg.like('Benzo', 'pop:102')
+    eg.actor_role('Benzo','pop:102', 'EVOLVED')
+    eg.actor_role('Washi', 'pop:101', 'AUTHORED')
+    eg.actor_role('Washi', 'pop:103', 'OWNS')
     eg.rate('Benzo', 'pop:101', 5)
     eg.rate('Washi', 'pop:101', 2)
     eg.follows('Washi','Benzo')
